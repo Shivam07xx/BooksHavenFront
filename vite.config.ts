45bf516e3225cd3820106e602b6c31817c6732f7
Mohammad Ramiz <ramizanas6@gmail.com>
2025-09-02 11:03:28 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://bookshavenserver.onrender.com',
         changeOrigin: true,
         secure: false,
       }
